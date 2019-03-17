54735239c3651ea1705ed6ca1fa7eb0ccf691a8c
j4sysiak <j4sysiak@gmail.com>
2019-03-17 19:31:04 +0100

    2

---
+++ spring-tutorial-Spring-Working-with-Databases-41/src/main/java/pl/jaceksysiak/spring/test/RandomText.java
@@ -0,0 +1,24 @@
+package pl.jaceksysiak.spring.test;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class RandomText {
+	
+	private static String[] texts = {
+		"I'll be back," +
+		"Get out!",
+		"I want your clothes, boots and motorcycle."
+	};
+	
+	public String getText(){
+		
+		Random random = new Random();
+		 
+		return texts[random.nextInt(texts.length)];
+	}
+
+}
